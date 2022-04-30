45d8e91af38fe89ef004e4c537337284241465fe
Ivan Biloš <ibilos00@fesb.hr>
2022-04-30 19:34:12 +0200

    Konflikt

--- Program/NewClass.cs
+++ Program/NewClass.cs
@@ -7,8 +7,16 @@ using System.Threading.Tasks;
 namespace Program
 {
 	internal class NewClass
-	{	
+	{
 		//Dodao sam ispis teksta novo umjesto prazne metode
 		Console.WriteLine("Novo");
+		static void Main(string[] args)
+		{
+		}
+		//Dodao sam ispis teksta novo umjesto prazne metode
+		Console.WriteLine("Novo");
+		static void Main(string[] args)
+		{
+		}
 	}
 }
